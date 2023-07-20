9f9c8a71e88a07f93310215f35880883afa13302
Junaid <junaid.saleem.10144@gmail.com>
2023-07-20 10:47:19 +0500

    mui themes & configurations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import { createTheme } from "@mui/material/styles"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
 
 function App() {
-  
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
-    <div className='app'>
-      
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   )
 }
